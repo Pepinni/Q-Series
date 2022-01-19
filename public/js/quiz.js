24641b0c8cdbe27d56079b17fe77712e7d1a7409
PIYUSH VERMA <b20163@students.iitmandi.ac.in>
2022-01-20 00:58:21 +0530

    Timer code refactored

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -9,67 +9,45 @@ $.get(path, function(DATA){
 	const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 	const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 	
-	const hrs = $('.hours');
-	const mins = $('.minutes');
-	const secs = $('.seconds');
-	const items = [hrs, mins, secs];
-	// let tempDate = new Date();
-	// let tempYear = tempDate.getFullYear();
-	// let tempMonth = tempDate.getMonth();
-	// let tempDay = tempDate.getDate();
-	// const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 	
-	let futureDate = new Date(2023, 3, 24, 11, 30, 0);
 	let ed = new Date(endDate);
-	// const year = futureDate.getFullYear();
-	// const hours = futureDate.getHours();
-	// const minutes = futureDate.getMinutes();
-	
-	// let month = futureDate.getMonth();
-	// month = months[month];
-	// const weekday = weekdays[futureDate.getDay()];
-	// const date = futureDate.getDate();
-	// giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
 	
 	const futureTime = ed.getTime();
 	function getRemainingTime() {
-	const today = new Date().getTime();
-	
-	const t = futureTime - today;
-	// const oneDay = 24 * 60 * 60 * 1000;
-	const oneHour = 60 * 60 * 1000;
-	const oneMinute = 60 * 1000;
-	// calculate all values
-	// let days = t / oneDay;
-	// days = Math.floor(days);
-	// let hours = Math.floor((t % oneDay) / oneHour);
-	let hours = Math.floor(t/oneHour);
-	let minutes = Math.floor((t % oneHour) / oneMinute);
-	let seconds = Math.floor((t % oneMinute) / 1000);
-	console.log(seconds);
-	// set values array
-	const values = [hours, minutes, seconds];
-	function format(item) {
-		if (item < 10) {
-		return (item = `0${item}`);
+		const today = new Date().getTime();
+		
+		const t = futureTime - today;
+		const oneHour = 60 * 60 * 1000;
+		const oneMinute = 60 * 1000;
+		let hours = Math.floor(t/oneHour);
+		let minutes = Math.floor((t % oneHour) / oneMinute);
+		let seconds = Math.floor((t % oneMinute) / 1000);
+		
+		// Changing the values in DOM
+		const hrs = $('.hours');
+		const mins = $('.minutes');
+		const secs = $('.seconds');
+		const items = [hrs, mins, secs];
+		const values = [hours, minutes, seconds];
+		function format(item) {
+			if (item < 10) {
+				return (item = `0${item}`);
+			}
+			return item;
 		}
-		return item;
-	}
-	
-	items.forEach(function (item, index) {
-		// item.innerHTML = format(values[index]);
+		items.forEach(function (item, index) {
+			// item.innerHTML = format(values[index]);
 		item.html(format(values[index]));
 	});
 
 	if (t < 0) {
 		clearInterval(countdown);
-		// deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
 		console.log("TIME OVER");
 	}
 	}
-	// countdown;
+
 	let countdown = setInterval(getRemainingTime, 1000);
-	//set initial values
+
 	getRemainingTime();
 })
 
